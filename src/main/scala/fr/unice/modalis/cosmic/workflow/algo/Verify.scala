766377d69302429044f51c03eba5ec7bf1d01cd6
cyrilcecchinel <cyril.cecchinel@gmail.com>
2014-11-03 17:27:15 +0100

    Check links (+ test) v1

---
+++ src/main/scala/fr/unice/modalis/cosmic/workflow/algo/Verify.scala
@@ -0,0 +1,32 @@
+package fr.unice.modalis.cosmic.workflow.algo
+
+import fr.unice.modalis.cosmic.workflow.core._
+
+/**
+ * Verify functions to check if a workflow is a correct one
+ * Created by Cyril Cecchinel - I3S Laboratory on 03/11/14.
+ */
+object Verify {
+
+  /**
+   * Check if a link is valid
+   * Authorized links
+   * Sensor --> Getter
+   * Getter --> Predicate
+   * Predicate --> Collector
+   * Predicate --> Predicate
+   *
+   * @param l Link
+   * @return Link validity status
+   */
+def checkLink(l:Link[_]):Boolean = (l.source.component, l.destination.component) match {
+  case (Source(_), PeriodicGetter(_)) => true
+  case (PeriodicGetter(_), Predicate(_)) => true
+  case (Predicate(_), Predicate(_)) => true
+  case (Predicate(_), Sink(_)) => true
+  case _ => false
+}
+
+
+
+}
---
+++ src/test/scala/fr/unice/modalis/cosmic/workflow/algo/VerifyTest.scala
@@ -0,0 +1,57 @@
+package fr.unice.modalis.cosmic.workflow.algo
+
+import fr.unice.modalis.cosmic.workflow.core._
+import org.specs2.mutable.SpecificationWithJUnit
+
+/**
+ * Created by Cyril Cecchinel - I3S Laboratory on 03/11/14.
+ */
+class VerifyTest extends SpecificationWithJUnit{
+
+  // Implementation scenario 2 : Fire prevention: a fire alert if a temperature threshold is reached
+  val validWF:Workflow[IntegerType] = {
+
+    val temperatureSensor = new Source[IntegerType]("TEMP_SENSOR")
+
+    val periodicGetter = new PeriodicGetter[IntegerType](30000)
+
+    val predicate = new Predicate[IntegerType](p => p.value > 50)
+
+    val collector = new Sink[IntegerType]("alice")
+
+    new Workflow[IntegerType]().addElement(temperatureSensor).addElement(periodicGetter).addElement(predicate).addElement(collector)
+    .addLink(new Link[IntegerType](temperatureSensor.output, periodicGetter.input)).addLink(new Link[IntegerType](periodicGetter.output, predicate.input))
+    .addLink(new Link[IntegerType](predicate.trueOutput,collector.input))
+
+  }
+
+  val invalidWF:Workflow[IntegerType] = {
+
+    val temperatureSensor = new Source[IntegerType]("TEMP_SENSOR")
+
+    val predicate = new Predicate[IntegerType](p => p.value > 50)
+
+    val collector = new Sink[IntegerType]("alice")
+
+    new Workflow[IntegerType]().addElement(temperatureSensor).addElement(predicate).addElement(collector)
+      .addLink(new Link[IntegerType](temperatureSensor.output, predicate.input))
+      .addLink(new Link[IntegerType](predicate.trueOutput,collector.input))
+
+  }
+
+  //END Scenario 2
+
+  //Links
+  val link1 = new Link[IntegerType](new Source[IntegerType]("TEST").output, new Predicate[IntegerType](p => p.value > 0).input) //Invalid
+
+  val link2 = new Link[IntegerType](new Source[IntegerType]("TEST").output, new PeriodicGetter[IntegerType](10000).input) //Valid
+
+  "A valid link must pass checkLink" in {
+    Verify.checkLink(link2) must beTrue
+  }
+
+  "A invalid link must failed" in {
+    Verify.checkLink(link1) must beFalse
+  }
+
+}
