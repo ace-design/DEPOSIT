aad51ee6b8bc5b94446bd1100458bbebfcdab98d
cyrilcecchinel <cyril.cecchinel@gmail.com>
2014-11-12 14:59:19 +0100

    Logic for predicates

---
+++ src/main/scala/fr/unice/modalis/cosmic/workflow/logic/guard/constraint/ValueConstraint.scala
@@ -0,0 +1,25 @@
+package fr.unice.modalis.cosmic.actions.guard.constraint
+
+import fr.unice.modalis.cosmic.workflow.core.DataType
+
+
+/**
+ * Value Constraint
+ * Fix a constraint upon a value  Value op threshold
+ * @constructor Create a new value constraint
+ * @param value Value to compare
+ * @param threshold Threshold
+ * @param operator Operator
+ */
+case class ValueConstraint[T<:DataType](val value: T, val threshold: Int, val operator: String) extends Constraint {
+
+  def isCorrect(o: String): Boolean = {
+    o == ">" || o == "<" || o == "==" || o == ">=" || o == "<=" || o == "!="
+  }
+
+  if (!isCorrect(operator)) throw new Exception("Bad operator for " + operator)
+
+  override def toString(): String = value.value + "\\" + operator + threshold
+
+
+}
