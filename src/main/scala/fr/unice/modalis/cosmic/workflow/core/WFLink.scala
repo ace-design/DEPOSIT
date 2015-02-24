30bff14f7c127157915cc510e29efa5fd1009529
cyrilcecchinel <cyril.cecchinel@gmail.com>
2015-02-24 16:30:22 +0100

    bug toString method

--- src/main/scala/fr/unice/modalis/cosmic/workflow/core/WFLink.scala
+++ src/main/scala/fr/unice/modalis/cosmic/workflow/core/WFLink.scala
@@ -12,7 +12,7 @@ case class WFLink[T<:DataType](source_output:Output[T], destination_input:Input[
   val source = source_output.parent
   val destination = destination_input.parent
 
-  override def toString:String = source_output + "-->" + source_output
+  override def toString:String = source_output + "-->" + destination_input
 
   override def equals(a:Any):Boolean = a match {
     case WFLink(a, b) if (a == source_output) && (b == destination_input) => true
