8dee905a08e5c65aabb82d6b37e1125003eca40e
cyrilcecchinel <cyril.cecchinel@gmail.com>
2015-02-25 10:46:26 +0100

    Auto connect

--- src/main/scala/fr/unice/modalis/cosmic/workflow/algo/Verify.scala
+++ src/main/scala/fr/unice/modalis/cosmic/workflow/algo/Verify.scala
@@ -3,14 +3,23 @@ package fr.unice.modalis.cosmic.workflow.algo
 import fr.unice.modalis.cosmic.workflow.core._
 
 /**
- * Created by Cyril Cecchinel - I3S Laboratory on 24/02/15.
+ * Verify methods useful (in)-validate a workflow
  */
 object Verify {
 
+  /**
+   * Get disconnected outputs in a workflow
+   * @param wf Workflow
+   * @return A set of all disconnected outputs
+   */
+ def getDisconnectedOutputs(wf:Workflow) = wf.allOutputs -- wf.links.map(_.source_output)
 
- def getNotConnectedOutputs(wf:Workflow) = wf.allOutputs -- wf.links.map(_.source_output)
-
-  def getNotConnectedInputs(wf:Workflow) = wf.allInputs -- wf.links.map(_.destination_input)
+  /**
+   * Get disconnected inputs in a workflow
+   * @param wf Workflow
+   * @return A set of all disconnected inputs
+   */
+  def getDisconnectedInputs(wf:Workflow) = wf.allInputs -- wf.links.map(_.destination_input)
 
 
 }
