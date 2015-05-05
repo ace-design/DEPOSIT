2ad3ed5a0b9a7d063a9bbc62c3175f684484cf1d
cyrilcecchinel <cyril.cecchinel@gmail.com>
2015-05-05 15:50:26 +0200

    Utils methods for creating sensors and collectors. close #9

---
+++ src/main/scala/fr/unice/modalis/cosmic/workflow/core/Utils.scala
@@ -0,0 +1,12 @@
+package fr.unice.modalis.cosmic.workflow.core
+
+/**
+ * Created by Cyril Cecchinel - I3S Laboratory on 05/05/15.
+ */
+object Utils {
+
+  def createEventSensors[T<:DataType](prefix:String, nb:Int = 1) = for (i <- 1 to nb) yield new EventSensor[T](prefix + i)
+  def createPeriodicSensors[T<:DataType](period:Int, prefix:String, nb:Int = 1) = for (i <- 1 to nb) yield new PeriodicSensor[T](period, prefix + i)
+  def createCollectors[T<:DataType](prefix:String, nb:Int = 1) = for (i <- 1 to nb) yield new Collector[T](prefix + i)
+
+}
---
+++ src/test/scala/fr/unice/modalis/cosmic/workflow/core/UtilsTest.scala
@@ -0,0 +1,28 @@
+package fr.unice.modalis.cosmic.workflow.core
+
+import org.specs2.mutable.SpecificationWithJUnit
+
+/**
+ * Created by Cyril Cecchinel - I3S Laboratory on 05/05/15.
+ */
+class UtilsTest extends SpecificationWithJUnit{
+
+  "Utils methods" should {
+    "Create n instances of Event sensors" in {
+      Utils.createEventSensors[IntegerType]("test_",10) must have size 10
+    }
+    "Create n instances of collector" in {
+      Utils.createCollectors[IntegerType]("test_",10) must have size 10
+    }
+    "Create n instances of Periodic sensors" in {
+      val res = Utils.createPeriodicSensors[IntegerType](5, "test_",10)
+      "check n instances" in {
+        res must have size 10
+      }
+      "check period" in {
+        res.forall(_.wishedPeriod == 5) must beTrue
+      }
+    }
+  }
+
+}
