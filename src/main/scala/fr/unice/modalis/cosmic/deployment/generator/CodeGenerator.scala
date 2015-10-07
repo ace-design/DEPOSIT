07d3b68627436a7469e51083ec33f2bd0d1904ca
Cyril Cecchinel <cyrilcecchinel@lyoko.local>
2015-10-07 10:45:37 +0200

    orderedGeneration list

--- src/main/scala/fr/unice/modalis/cosmic/deployment/generator/CodeGenerator.scala
+++ src/main/scala/fr/unice/modalis/cosmic/deployment/generator/CodeGenerator.scala
@@ -1,8 +1,15 @@
 package fr.unice.modalis.cosmic.deployment.generator
 
+import fr.unice.modalis.cosmic.deposit.core.Policy
+import org.chocosolver.solver.Solver
+import org.chocosolver.solver.constraints.IntConstraintFactory
+import org.chocosolver.solver.variables.VariableFactory
+
 import scala.collection.mutable
 
 /**
+ * Code generator trait
+ * Contains common methods shared by all code generators
  * Created by Cyril Cecchinel - I3S Laboratory on 05/10/15.
  */
 trait CodeGenerator {
@@ -12,7 +19,30 @@ trait CodeGenerator {
 
   val functions = mutable.HashMap[String,String]()
   val variables = mutable.Set[Variable]()
+
+  def orderedGenerationList(p:Policy) = {
+    val totalConcepts = p.ios.size + p.operations.size
+    val solver = new Solver("Generation ordering problem")
+    val sourcesvariables = for (s <- p.sources) yield { VariableFactory.fixed(s.id, 1, solver)}
+    val operationvariables = for (o <- p.operations) yield { VariableFactory.bounded(o.id, 2, totalConcepts, solver)}
+    val collectorvariables = for (c <- p.collectors) yield {VariableFactory.fixed(c.id, totalConcepts, solver)}
+
+    val variables = sourcesvariables ++ operationvariables ++ collectorvariables
+
+    for (l <- p.links) yield {solver.post(IntConstraintFactory.arithm(variables.find(_.getName equals l.source.id).get, "<", variables.find(_.getName equals l.destination.id).get))}
+    solver.post(IntConstraintFactory.alldifferent(operationvariables.toArray))
+    
+    if (solver.findSolution()) {
+      val namedOperationsOrder = solver.retrieveIntVars().map(v => (v.getValue, v.getName)).toList.sortBy(_._1).map(_._2).map(p.findConceptById(_).get)
+      p.sources.toList ++ namedOperationsOrder ++ p.collectors.toList
+    }
+    else {
+      throw new NonGenerableException(p)
+    }
+
+  }
 }
 
 case class NonHandledSensorException(io:Any) extends Exception("Non handled sensor " + io)
 
+case class NonGenerableException(p:Policy) extends Exception(p.name + " is not generable")
