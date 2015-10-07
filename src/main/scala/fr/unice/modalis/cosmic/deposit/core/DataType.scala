0151a1da8248f471466afca34c2e6223027f26bc
Cyril Cecchinel <cyrilcecchinel@lyoko.local>
2015-10-07 16:18:32 +0200

    case class datatypes

--- src/main/scala/fr/unice/modalis/cosmic/deposit/core/DataType.scala
+++ src/main/scala/fr/unice/modalis/cosmic/deposit/core/DataType.scala
@@ -20,35 +20,35 @@ trait CompositeType extends DataType {
 /**
  * Represent Integer values
  */
-class IntegerType(val value:Int) extends AtomicType {
+case class IntegerType(val value:Int) extends AtomicType {
   override val name: String = "int"
 }
 
 /**
  * Represent Double values
  */
-class DoubleType(val value:Double) extends AtomicType {
+case class DoubleType(val value:Double) extends AtomicType {
   override val name: String = "double"
 }
 
 /**
  * Represent Long values
  */
-class LongType(val value:Long) extends AtomicType {
+case class LongType(val value:Long) extends AtomicType {
   override val name: String = "long"
 }
 
 /**
  * Represent String values
  */
-class StringType(val value:String) extends AtomicType {
+case class StringType(val value:String) extends AtomicType {
   override val name: String = "String"
 }
 
 /**
  * Represent SmartCampus Sensor Data
  */
-class SmartCampusType(val value:(String, Int, Long)) extends CompositeType {
+case class SmartCampusType(val value:(String, Int, Long)) extends CompositeType {
   val bindings = Map("n" -> new StringType(value._1),
                      "v" -> new IntegerType(value._2),
                      "t" -> new LongType(value._3))
@@ -57,7 +57,7 @@ class SmartCampusType(val value:(String, Int, Long)) extends CompositeType {
 /**
  * Represent Santander Parking Sensor Data
  */
-class SantanderParkingType(val value:(Int, String, Double, Int)) extends CompositeType {
+case class SantanderParkingType(val value:(Int, String, Double, Int)) extends CompositeType {
   val bindings = Map("nodeId" -> new IntegerType(value._1),
                      "date" -> new StringType(value._2),
                      "battery" -> new DoubleType(value._3),
