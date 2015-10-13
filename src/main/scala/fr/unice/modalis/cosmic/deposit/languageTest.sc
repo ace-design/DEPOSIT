b8c2e47e074ccba446b17bb41309cf6816429c25
Cyril Cecchinel <cyrilcecchinel@lyoko.local>
2015-10-13 15:23:47 +0200

    Update Scenarios

--- src/main/scala/fr/unice/modalis/cosmic/deposit/languageTest.sc
+++
@@ -1,6 +0,0 @@
-import fr.unice.modalis.cosmic.deposit.core.Extract
-
-val e = Extract("test")
-e.field
-e.commonName
-
--- src/main/scala/fr/unice/modalis/cosmic/deposit/scenarios/DemoCodeGeneration.scala
+++ src/main/scala/fr/unice/modalis/cosmic/deposit/scenarios/DemoCodeGeneration.scala
@@ -1,7 +1,7 @@
 package fr.unice.modalis.cosmic.deposit.scenarios
 
 import fr.unice.modalis.cosmic.deployment.PreDeploy
-import fr.unice.modalis.cosmic.deposit.converter.ToGraphviz
+import fr.unice.modalis.cosmic.deployment.generator.ArduinoGenerator
 import fr.unice.modalis.cosmic.deposit.core._
 
 /**
@@ -14,31 +14,31 @@ object DemoCodeGeneration extends App{
   val convert_workflow = {
 
     val s = GenericInput("parking_sensor", classOf[SantanderParkingType])
-    val constant = Constant(new IntegerType(1), classOf[IntegerType])
     val a1 = Extract("status", classOf[SantanderParkingType], classOf[IntegerType])
     val a2 = Conditional("i == 1", classOf[IntegerType])
-    val a3a = Sub(Set("i1", "i2"), classOf[IntegerType])
-    val a3b = Add(Set("i1", "i2"), classOf[IntegerType])
+
+    val a3a = Increment(IntegerType(-1), classOf[IntegerType])
+    val a3b = Increment(IntegerType(+1), classOf[IntegerType])
 
     val place_status = GenericOutput("place_status", classOf[IntegerType])
 
     val l1 = new Link(s.output, a1.input)
     val l2 = new Link(a1.output, a2.input)
 
-    val l3a = new Link(a2.thenOutput, a3a.getInput("i1"))
-    val l3b = new Link(constant.output, a3a.getInput("i2"))
+    val l3a = new Link(a2.thenOutput, a3a.input)
 
-    val l4a = new Link(a2.elseOutput, a3b.getInput("i1"))
-    val l4b = new Link(constant.output, a3b.getInput("i2"))
+
+    val l4a = new Link(a2.elseOutput, a3b.input)
+
 
     val l5a = new Link(a3a.output, place_status.input)
     val l5b = new Link(a3b.output, place_status.input)
 
-    new Policy("convert", Set(s,place_status), Set(a1, a2, a3a, a3b, constant), Set(l1, l2, l3a, l3b, l4a, l4b, l5a, l5b))
+    new Policy("convert", Set(s,place_status), Set(a1, a2, a3a, a3b), Set(l1, l2, l3a, l4a, l5a, l5b))
 
   }
 
-  val s1 = EventSensor("s1", classOf[SantanderParkingType])
+  val s1 = EventSensor("TEST", classOf[SantanderParkingType])
   val s2 = EventSensor("s2", classOf[SantanderParkingType])
   val c = Collector("collector", classOf[IntegerType])
 
@@ -59,8 +59,14 @@ object DemoCodeGeneration extends App{
 
   val policy_expanded = PreDeploy.expandProcesses(policy)
 
-  println(ToGraphviz(policy_expanded))
+  //println(ToGraphviz(policy_expanded))
   //println(ArduinoGenerator.orderedGenerationList(policy_expanded))
 
+println(ArduinoGenerator(policy_expanded))
+//  val res = ArduinoGenerator.generateInstructionList(policy_expanded).flatMap(i => Set(i.inputs, i.outputs)).flatten
+
+  //println(ArduinoGenerator.generateVariablesDeclaration(res.toSet))
+  /*println(ArduinoGenerator.generateUpdateMethod(policy_expanded))
+  println(ArduinoGenerator.generateInstructionList(policy_expanded).foldLeft(""){(acc, e) => acc + e.body + "\n"})*/
 
 }
