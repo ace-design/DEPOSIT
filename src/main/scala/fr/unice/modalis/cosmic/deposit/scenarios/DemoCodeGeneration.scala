25bbdb79e383d83526184a3519c0faccffbdf63c
Cyril Cecchinel <cyrilcecchinel@lyoko.local>
2015-10-22 17:36:17 +0200

    Demo Ready with 3 sensors

--- src/main/scala/fr/unice/modalis/cosmic/deposit/scenarios/DemoCodeGeneration.scala
+++ src/main/scala/fr/unice/modalis/cosmic/deposit/scenarios/DemoCodeGeneration.scala
@@ -1,6 +1,5 @@
 package fr.unice.modalis.cosmic.deposit.scenarios
 
-import fr.unice.modalis.cosmic.deployment.PreDeploy
 import fr.unice.modalis.cosmic.deployment.generator.ArduinoGenerator
 import fr.unice.modalis.cosmic.deposit.core._
 
@@ -10,63 +9,17 @@ import fr.unice.modalis.cosmic.deposit.core._
 //noinspection ScalaDefaultFileTemplateUsage
 object DemoCodeGeneration extends App{
 
-  // Define the process
-  val convert_workflow = {
+  val s1 = EventSensor("BUTTON1", classOf[SmartCampusType])
+  val s2 = EventSensor("BUTTON2", classOf[SmartCampusType])
+  val s3 = EventSensor("BUTTON3", classOf[SmartCampusType])
+  val add = Add(Set("s1", "s2", "s3"), classOf[SmartCampusType], Some("PARKING_COUNT"))
+  val c = Collector("output", classOf[SmartCampusType])
+  val l1 = Link(s1.output, add.getInput("s1"))
+  val l2 = Link(s2.output, add.getInput("s2"))
+  val l3 = Link(s3.output, add.getInput("s3"))
+  val l4 = Link(add.output, c.input)
+  val p = new Policy("demo").add(s1).add(s2).add(s3).add(add).add(c).addLink(l1).addLink(l2).addLink(l3).addLink(l4)
 
-    val s = GenericInput("parking_sensor", classOf[SantanderParkingType])
-    val a1 = Extract("status", classOf[SantanderParkingType], classOf[IntegerType])
-    val a2 = Conditional("i == 1", classOf[IntegerType])
-
-    val a3a = Increment(IntegerType(-1), classOf[IntegerType])
-    val a3b = Increment(IntegerType(+1), classOf[IntegerType])
-
-    val place_status = GenericOutput("place_status", classOf[IntegerType])
-
-    val l1 = new Link(s.output, a1.input)
-    val l2 = new Link(a1.output, a2.input)
-
-    val l3a = new Link(a2.thenOutput, a3a.input)
-
-
-    val l4a = new Link(a2.elseOutput, a3b.input)
-
-
-    val l5a = new Link(a3a.output, place_status.input)
-    val l5b = new Link(a3b.output, place_status.input)
-
-    new Policy("convert", Set(s,place_status), Set(a1, a2, a3a, a3b), Set(l1, l2, l3a, l4a, l5a, l5b))
-
-  }
-
-  val s1 = EventSensor("TEST", classOf[SantanderParkingType])
-  val s2 = EventSensor("s2", classOf[SantanderParkingType])
-  val c = Collector("collector", classOf[IntegerType])
-
-  val p1 = Process(convert_workflow, classOf[SantanderParkingType], classOf[IntegerType])
-  val p2 = Process(convert_workflow, classOf[SantanderParkingType], classOf[IntegerType])
-
-  val add = Add(Set("i1", "i2"), classOf[IntegerType])
-
-  val l1 = Link(s1.output, p1.getInput("parking_sensor"))
-  val l2 = Link(s2.output, p2.getInput("parking_sensor"))
-
-  val l3 = Link(p1.getOutput("place_status"), add.getInput("i1"))
-  val l4 = Link(p2.getOutput("place_status"), add.getInput("i2"))
-  val l5 = Link(add.output, c.input)
-
-  val policy = Policy("demo", Set(s1, s2, c), Set(p1, p2, add), Set(l1, l2, l3, l4, l5))
-
-
-  val policy_expanded = PreDeploy.expandProcesses(policy)
-
-  //println(ToGraphviz(policy_expanded))
-  //println(ArduinoGenerator.orderedGenerationList(policy_expanded))
-
-println(ArduinoGenerator(policy_expanded))
-//  val res = ArduinoGenerator.generateInstructionList(policy_expanded).flatMap(i => Set(i.inputs, i.outputs)).flatten
-
-  //println(ArduinoGenerator.generateVariablesDeclaration(res.toSet))
-  /*println(ArduinoGenerator.generateUpdateMethod(policy_expanded))
-  println(ArduinoGenerator.generateInstructionList(policy_expanded).foldLeft(""){(acc, e) => acc + e.body + "\n"})*/
+  println(ArduinoGenerator(p))
 
 }
