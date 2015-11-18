2dfdaabfa9bb237ad4fdf3b7632b826e9b1b0f5b
Cyril Cecchinel <ulrich06@users.noreply.github.com>
2015-11-18 17:09:56 +0100

    Temperature converter demo

---
+++ src/main/scala/fr/unice/modalis/cosmic/demos/DemoCelsiusToFahrenheit.scala
@@ -0,0 +1,38 @@
+package fr.unice.modalis.cosmic.demos
+
+import fr.unice.modalis.cosmic.deployment.PreDeploy
+import fr.unice.modalis.cosmic.deployment.generator.ArduinoGenerator
+import fr.unice.modalis.cosmic.deployment.utils.InfrastructureModelBuilder
+import fr.unice.modalis.cosmic.deposit.core._
+
+
+/**
+  * This demonstration illustrates how a process can be used to convert
+  * a celsius temperature into a fahrenheit temperature
+  *
+  * Concepts demonstrated :
+  *           Process reuse
+  *
+  * Created by Cyril Cecchinel - I3S Laboratory on 18/11/2015.
+  */
+object DemoCelsiusToFahrenheit extends App{
+
+  val sensor = PeriodicSensor(2, "AC_443", classOf[SmartCampusType])
+  val convert = Process(StandardizedPolicies.celsiusToFahrenheit, classOf[SmartCampusType], classOf[SmartCampusType])
+  val collectorfahrenheit = Collector("collectorFarenheit", classOf[SmartCampusType])
+  val collectorcelsius = Collector("collectorCelsius", classOf[SmartCampusType])
+
+  val l1 = Link(sensor.output, convert.getInput("celsius"))
+  val l2 = Link(convert.getOutput("fahrenheit"), collectorfahrenheit.input)
+  val l3 = Link(sensor.output, collectorcelsius.input)
+
+  val p = new Policy("CelsiusToFarenheit")
+    .add(sensor).add(convert).add(collectorcelsius).add(collectorfahrenheit)
+    .addLink(l1).addLink(l2).addLink(l3)
+
+  val topology = InfrastructureModelBuilder("assets/configurations/smartcampus_xbeenetwork.xml")
+  val predeployed = PreDeploy(p, topology)
+
+  ArduinoGenerator(predeployed, toFile = true)
+
+}
--- src/main/scala/fr/unice/modalis/cosmic/demos/StandardizedPolicies.scala
+++ src/main/scala/fr/unice/modalis/cosmic/demos/StandardizedPolicies.scala
@@ -8,6 +8,10 @@ import fr.unice.modalis.cosmic.deposit.core._
   */
 object StandardizedPolicies {
 
+   /*
+    * This policy aims at convert a raw sensor (ie. magnetic switch) to an opening detection sensor.
+    * Input: input / Output: {open,closed}
+    */
   val rawValueToOpeningSensor = {
     val input = GenericInput("input", classOf[SmartCampusType])
     val filtering = Conditional("value < 500", classOf[SmartCampusType])
@@ -21,5 +25,46 @@ object StandardizedPolicies {
     new Policy().add(input).add(filtering).add(open).add(closed)
                 .addLink(l1).addLink(l2).addLink(l3)
     
+  }
+
+   /*
+    * This policy aims at convert a celsius value to a fahrenheit value
+    * Input: celsius / Output: fahrenheit
+    */
+  val celsiusToFahrenheit = {
+    val input = GenericInput("celsius", classOf[SmartCampusType])
+    val multiply = Multiply(IntegerType(9),classOf[SmartCampusType])
+    val divide = Divide(IntegerType(5), classOf[SmartCampusType])
+    val add = Increment(IntegerType(32), classOf[SmartCampusType])
+    val output = GenericOutput("fahrenheit", classOf[SmartCampusType])
+
+    val l1 = Link(input.output, multiply.input)
+    val l2 = Link(multiply.output, divide.input)
+    val l3 = Link(divide.output, add.input)
+    val l4 = Link(add.output, output.input)
+
+    new Policy().add(input).add(multiply).add(divide).add(add).add(output)
+                .addLink(l1).addLink(l2).addLink(l3).addLink(l4)
+
+  }
+
+    /*
+    * This policy aims at convert a celsius value to a fahrenheit value
+    * Input: fahrenheit / Output: celsius
+    */
+  val fahrenheitToCelsius = {
+    val input = GenericInput("fahrenheit", classOf[SmartCampusType])
+    val sub = Increment(IntegerType(-32), classOf[SmartCampusType])
+    val multiply = Multiply(IntegerType(5), classOf[SmartCampusType])
+    val divide = Divide(IntegerType(9), classOf[SmartCampusType])
+    val output = GenericOutput("celsius", classOf[SmartCampusType])
+
+    val l1 = Link(input.output, sub.input)
+    val l2 = Link(sub.output, multiply.input)
+    val l3 = Link(multiply.output, divide.input)
+    val l4 = Link(divide.output, output.input)
+
+    new Policy().add(input).add(sub).add(multiply).add(divide).add(output)
+                .addLink(l1).addLink(l2).addLink(l3).addLink(l4)
   }
 }
