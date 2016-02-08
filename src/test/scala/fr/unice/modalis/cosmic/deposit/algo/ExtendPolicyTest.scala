f004373261880860975b24805a8826ecfd9929d0
Cyril Cecchinel <ulrich06@users.noreply.github.com>
2016-02-08 16:26:24 +0100

    Update tests

--- src/test/scala/fr/unice/modalis/cosmic/deposit/algo/ExtendPolicyTest.scala
+++ src/test/scala/fr/unice/modalis/cosmic/deposit/algo/ExtendPolicyTest.scala
@@ -1,5 +1,6 @@
 package fr.unice.modalis.cosmic.deposit.algo
 
+import fr.unice.modalis.cosmic.ComprehensivePolicy
 import fr.unice.modalis.cosmic.deposit.core._
 import org.specs2.mutable.SpecificationWithJUnit
 
@@ -29,17 +30,15 @@ class ExtendPolicyTest extends SpecificationWithJUnit {
   }
 
   "An expendable policy" should {
-    "provide join points for all expendable operations" in pending("Waiting to be adapted"){
-      ExtendPolicy(DCPTest.convert_workflow).ios.collect {case x:JoinPoint[_] => x} must have size 12
+    "provide join points for all expendable operations" in {
+      ExtendPolicy(ComprehensivePolicy.innerPolicy()).ios.collect {case x:JoinPoint[_] => x} must have size 11
     }
-    "provide join points for all expendable operations (2)" in pending("Waiting to be adapted"){
-      ExtendPolicy(DCPTest.convert_workflow2).ios.collect {case x:JoinPoint[_] => x} must have size 11
-    }
+
   }
 
   "A factorized policy" should {
     "not contain any Join Points" in {
-      FactorizePolicy(ExtendPolicy(DCPTest.convert_workflow)).ios.collect {case x:JoinPoint[_] => x} must have size 0
+      FactorizePolicy(ComprehensivePolicy.innerPolicy()).ios.collect {case x:JoinPoint[_] => x} must have size 0
     }
 
   }
