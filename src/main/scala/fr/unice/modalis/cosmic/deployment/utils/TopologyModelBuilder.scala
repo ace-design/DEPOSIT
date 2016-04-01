177fadafe5a40d56e07e55df6611eae250380986
Cyril Cecchinel <ulrich06@users.noreply.github.com>
2016-04-01 10:22:37 +0200

    Separate the file loading from the execution of the proceeding

--- src/main/scala/fr/unice/modalis/cosmic/deployment/utils/TopologyModelBuilder.scala
+++ src/main/scala/fr/unice/modalis/cosmic/deployment/utils/TopologyModelBuilder.scala
@@ -4,7 +4,7 @@ import fr.unice.modalis.cosmic.deployment.infrastructure.Features.{EntityComputa
 import fr.unice.modalis.cosmic.deployment.infrastructure.{Features, NetworkTopology}
 import fr.unice.modalis.cosmic.deployment.network.{Communication, Edge, Entity, Sensor}
 
-import scala.xml.{NodeSeq, XML}
+import scala.xml.{Elem, NodeSeq, XML}
 
 
 /**
@@ -18,7 +18,7 @@ object TopologyModelBuilder {
     * @param pathToFile Path to XML representation
     * @return a Network Topology object
     */
-  def apply(pathToFile:String) = loadFromSpineFM(pathToFile)
+  def apply(pathToFile: String) = loadFromSpineFM(pathToFile)
 
   /**
     * Find a feature into an XML representation
@@ -26,16 +26,16 @@ object TopologyModelBuilder {
     * @param seq XML representation
     * @return Feature found ?
     */
-  def findFeature(name:String, seq:NodeSeq) = (seq \\ "feature").exists(_.text.toLowerCase == name.toLowerCase)
+  def findFeature(name: String, seq: NodeSeq) = (seq \\ "feature").exists(_.text.toLowerCase == name.toLowerCase)
+
 
   /**
     * Load the configuration
-    * @param pathToFile Path to XML representation
+    * @param configuration XML representation
     * @return a Network Topology object
     */
-  def loadFromSpineFM(pathToFile: String) = {
-
-    def buildSensor(name: String, pin:Option[String], s: NodeSeq) = {
+  def loadFromSpineFM(configuration: Elem):NetworkTopology = {
+    def buildSensor(name: String, pin: Option[String], s: NodeSeq) = {
       // Get type
       val sensorType = (for (k <- Features.featureTypeAssociation.keys if findFeature(k, s \\ "features")) yield Features.featureTypeAssociation(k)).head
       val sensorBrand = (for (k <- Features.featureBrandAssociation.keys if findFeature(k, s \\ "features")) yield Features.featureBrandAssociation(k)).head
@@ -43,7 +43,7 @@ object TopologyModelBuilder {
       new Sensor(name, sensorType, sensorBrand, pin)
     }
 
-    def buildEntity(id: String, sensors: Seq[Sensor], computation: EntityComputation.Value, etype: EntityType.Value, s:NodeSeq) = {
+    def buildEntity(id: String, sensors: Seq[Sensor], computation: EntityComputation.Value, etype: EntityType.Value, s: NodeSeq) = {
 
       def buildCommunications(seq: NodeSeq) = {
         for (c <- seq \\ "communication") yield {
@@ -54,15 +54,12 @@ object TopologyModelBuilder {
       }
 
       val entityPower = (for (k <- Features.featurePowerAssociation.keys if findFeature(k, s \\ "powers" \\ "power")) yield Features.featurePowerAssociation(k)).head
-      val entityCommunications = buildCommunications (s \\ "communications")
+      val entityCommunications = buildCommunications(s \\ "communications")
       val entityLanguages = (for (k <- Features.featureLanguageAssociation.keys if findFeature(k, s \\ "languages" \\ "language")) yield Features.featureLanguageAssociation(k)).head
       val entityPublicUrl = s \@ "remote"
-      new Entity(id,sensors.toSet,entityPower, entityCommunications.toSet, computation, etype, entityLanguages, if (!entityPublicUrl.isEmpty) Some(entityPublicUrl) else None)
+      new Entity(id, sensors.toSet, entityPower, entityCommunications.toSet, computation, etype, entityLanguages, if (!entityPublicUrl.isEmpty) Some(entityPublicUrl) else None)
     }
 
-
-
-    val configuration = XML.loadFile(pathToFile)
 
     val name = (configuration \\ "sensornetwork" \ "@id").text
 
@@ -74,7 +71,7 @@ object TopologyModelBuilder {
                         etype = EntityType.withName((e \\ "@type").text)) yield {
 
 
-      val sensors = for (s <- e \\ "sensors" \\ "sensor"; sid = (s \ "@id").text; pin = s \@ "pin"; spin = if(pin.isEmpty) None else Some(pin)) yield buildSensor(sid, spin, s)
+      val sensors = for (s <- e \\ "sensors" \\ "sensor"; sid = (s \ "@id").text; pin = s \@ "pin"; spin = if (pin.isEmpty) None else Some(pin)) yield buildSensor(sid, spin, s)
 
       buildEntity(id, sensors, computation, etype, e)
     }
@@ -85,9 +82,12 @@ object TopologyModelBuilder {
                      if source.isDefined && target.isDefined) yield Edge(source.get, target.get)
 
     new NetworkTopology(name, entities.toSet, edges.toSet)
-
-
-
-
   }
+
+  /**
+    * Load the configuration
+    * @param pathToFile Path to XML representation
+    * @return a Network Topology object
+    */
+  def loadFromSpineFM(pathToFile: String):NetworkTopology = loadFromSpineFM(XML.loadFile(pathToFile))
 }
