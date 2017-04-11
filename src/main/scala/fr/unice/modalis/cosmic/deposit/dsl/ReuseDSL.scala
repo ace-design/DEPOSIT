af1971a8b726623b5b1c8b7bb9a6793c18396c8c
Cyril Cecchinel <ulrich06@users.noreply.github.com>
2017-04-11 16:22:15 +0200

    dsl/weave

--- src/main/scala/fr/unice/modalis/cosmic/deposit/dsl/ReuseDSL.scala
+++ src/main/scala/fr/unice/modalis/cosmic/deposit/dsl/ReuseDSL.scala
@@ -1,6 +1,7 @@
 package fr.unice.modalis.cosmic.deposit.dsl
 
-import fr.unice.modalis.cosmic.deposit.core.Policy
+import fr.unice.modalis.cosmic.deposit.algo.{ExtendPolicy, Unification, Weave}
+import fr.unice.modalis.cosmic.deposit.core._
 
 /**
   * Created by Cyril Cecchinel - I3S Laboratory on 11/04/2017.
@@ -12,11 +13,34 @@ trait ReuseDSL {
     currentSelection.get
   }
 
+  def weaveBetween(policy1: Policy, policy2: Policy) = {
+    currentWeave = Some(WeaveBuilder(policy1, policy2))
+    currentWeave.get
+  }
+
   case class SelectBuilder(policy: Policy){
     def conceptsMarked(markers:String*) = {
       policy.select(policy.operations.filter(_._marker.isDefined).filter(o => markers.contains(o._marker.get)).toSet, "select_" + policy.name)
     }
   }
 
+  case class WeaveBuilder(policy1: Policy, policy2: Policy){
+    val exPolicy1 = ExtendPolicy(policy1)
+    val exPolicy2 = ExtendPolicy(policy2)
+
+    def andAssociates(associations:((String,String) , (String,String))*) = {
+      val unifications = associations.map{ association =>
+        val jpoutput = exPolicy1.outputJoinPoints.find(p => p.fromConceptOutput.name == association._1._2 && p.fromConceptOutput.parent.asInstanceOf[Operation[_,_]]._marker.get == association._1._1)
+        val jpinput = exPolicy2.inputJoinPoints.find(p => p.toConceptInput.name == association._2._2 && p.toConceptInput.parent.asInstanceOf[Operation[_,_]]._marker.get == association._2._1)
+        if (jpinput.isDefined && jpoutput.isDefined){
+          Unification(jpoutput.get.asInstanceOf[JoinPointOutput[DataType]], jpinput.get.asInstanceOf[JoinPointInput[DataType]])
+        } else throw new Exception("Join point not found")
+      }
+      Weave(exPolicy1, exPolicy2, unifications.toSet)
+    }
+  }
+
   protected var currentSelection: Option[SelectBuilder] = None
+
+  protected var currentWeave: Option[WeaveBuilder] = None
 }
